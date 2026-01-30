a50d0a6fe76c6dc15fef049883037c09c699bdb2
Dinh Hoang <dinhminhhoang235@gmail.com>
2026-01-30 12:11:47 +0700

    fixbug

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
+  },
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === 'production'
+        ? { exclude: ['error', 'warn'] }
+        : false,
   },
 }
 
